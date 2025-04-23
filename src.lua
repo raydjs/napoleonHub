758a80787bc9a087b9d6c580b39304c158cd9092
raydjs <68088905+raydjs@users.noreply.github.com>
2025-04-22 21:13:08 -0600

    Create src.lua

---
+++ src.lua
@@ -0,0 +1,89 @@
+if napoleonLoaded then
+    return
+end
+
+pcall(function()
+    getgenv().napoleonLoaded = true
+end)
+
+local api = loadstring(
+    game:HttpGet('https://sdkapi-public.luarmor.net/library.lua')
+)()
+
+local games = if ID == 73956553001240 or ID == 134314141048307 or ID == 109684591839194 or ID == 86789627188240 or ID == 6931042565 or ID == 74691681039273 then
+  -- // Volleyball legends
+ '10231c45388ada5c77add5a7583a2b19'
+    else
+     nil
+
+if games == nil then
+    return
+end
+
+api.script_id = games
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local linkvertise = 'https://ads.luarmor.net/get_key?for=hubKey-bbTFbWlaffcB'
+local keyFile = 'NAPOLEON_IS_THE_GREATEST_KEY.txt'
+
+local Window = Rayfield:CreateWindow({
+    Name = 'Napoleon',
+    LoadingTitle = 'bonaparte.',
+    LoadingSubtitle = 'by jvjir',
+    Discord = {
+        Enabled = true,
+        Invite = 'bWzCFPk83g',
+        RememberJoins = true,
+    },
+})
+
+local function getKey()
+    if isfile(keyFile) then
+        return readfile(keyFile)
+    end
+end
+
+local key = getKey()
+
+if key then
+    local status = api.check_key(script_key or key)
+
+    if status.code == 'KEY_VALID' then
+        script_key = script_key or key
+
+        task.delay(2, function()
+            Rayfield:Destroy()
+        end)
+        api.load_script()
+    else
+        return
+    end
+end
+
+local tabKey = Window:CreateTab('Key', 4483362458)
+local Input = tabKey:CreateInput({
+    Name = 'Key Here',
+    PlaceholderText = '...',
+    OnEnter = true,
+    Callback = function(key)
+        local status = api.check_key(key)
+
+        if status.code == 'KEY_VALID' then
+            writefile(keyFile, key)
+            script_key = script_key or key
+            Rayfield:Destroy()
+            api.load_script()
+        else
+            print(status.code)
+        end
+    end,
+})
+
+tabKey:CreateLabel('press enter to submit\ntap anywhere to submit')
+tabKey:CreateButton({
+    Name = 'Get key (linkvertise.com)',
+    Callback = function()
+        setclipboard(linkvertise)
+    end,
+})
