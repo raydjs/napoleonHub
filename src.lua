1e3ba7522f459e7ab530ce3326d91f2d83724965
raydjs <68088905+raydjs@users.noreply.github.com>
2025-04-27 16:54:47 -0600

    Update src.lua

--- src.lua
+++ src.lua
@@ -10,24 +10,22 @@ local api = loadstring(
     game:HttpGet('https://sdkapi-public.luarmor.net/library.lua')
 )()
 
+local TeleportService: TeleportService = cloneref(game:GetService("TeleportService"))
+
+local repo = 'https://raw.githubusercontent.com/raydjs/Obsidian/main/'
+local discord_link = 'discord.gg/bWzCFPk83g'
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+
 local ID = game.PlaceId
 
 local games = if ID == 73956553001240 or ID == 134314141048307 or ID == 109684591839194 or ID == 86789627188240 or ID == 6931042565 or ID == 74691681039273 or ID == 79136731857959 then
-  -- // Volleyball legends
- '10231c45388ada5c77add5a7583a2b19'
-    else
-     nil
-
-if games == nil then
-    return print(games)
-end
-
+    -- // Volleyball legends
+   '10231c45388ada5c77add5a7583a2b19'
+      else
+       nil
+       
 api.script_id = games
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
-local linkvertise = 'https://ads.luarmor.net/get_key?for=hubKey-bbTFbWlaffcB'
-local lootlab = 'https://ads.luarmor.net/get_key?for=lootLab-KHgdrTasHCWU'
 local keyFile = 'NAPOLEON_IS_THE_GREATEST_KEY.txt'
 
 local function getKey()
@@ -43,54 +41,80 @@ local status = api.check_key(script_key or key)
 if status.code == 'KEY_VALID' then
     script_key = script_key or key
 
-    task.delay(2, function()
-        Rayfield:Destroy()
-    end)
+    Library:Unload()
+    Library:Notify("Key is valid", 4)
     api.load_script()
     return
 end
 
-local Window = Rayfield:CreateWindow({
-    Name = 'Napoleon',
-    LoadingTitle = 'bonaparte.',
-    LoadingSubtitle = 'by jvjir',
-    Discord = {
-        Enabled = true,
-        Invite = 'bWzCFPk83g',
-        RememberJoins = true,
-    },
+local Window = Library:CreateWindow({
+	Title = "Napoleon Hub",
+	Footer = discord_link,
+	Icon = 105747086514734,
+	NotifySide = "Right",
+	ShowCustomCursor = false,
+    MobileButtonsSide = "Left",
 })
 
-local tabKey = Window:CreateTab('Key', 4483362458)
-local Input = tabKey:CreateInput({
-    Name = 'Key Here',
-    PlaceholderText = '...',
-    OnEnter = true,
-    Callback = function(key)
-        local status = api.check_key(key)
+local Tabs = {
+    Key = Window:AddKeyTab("Key System"),
+}
 
-        if status.code == 'KEY_VALID' then
-            writefile(keyFile, key)
-            script_key = script_key or key
-            Rayfield:Destroy()
-            api.load_script()
-        else
-            print(status.code)
-        end
+Tabs.Key:AddLabel({
+	Text = "Enter Key Here",
+	DoesWrap = true,
+	Size = 16,
+})
+
+Tabs.Key:AddKeyBox(function(_, ReceivedKey)
+    local status = api.check_key(ReceivedKey)
+
+    if status.code == 'KEY_VALID' then
+        writefile(keyFile, ReceivedKey)
+        script_key = script_key or ReceivedKey
+        api.load_script()
+        Library:Notify("SUCCESS!", 4)
+        Library:Unload()
+    else
+        Library:Notify("INVALID", 4)
+    end
+end)
+
+Tabs.Key:AddButton({
+    Text = "Get Key (Linkvertise)",
+    Func = function()
+        setclipboard('https://ads.luarmor.net/get_key?for=hubKey-bbTFbWlaffcB')
+        Library:Notify({
+            Title = "Linkvertise has been copied!",
+            Description = "Complete the link to get Key."
+        }, 4)
     end,
 })
 
-tabKey:CreateLabel('press enter to submit\ntap anywhere to submit')
-tabKey:CreateButton({
-    Name = 'Get key (linkvertise.com) (it will copy. Paste the link in browser)',
-    Callback = function()
-        setclipboard(linkvertise)
+Tabs.Key:AddButton({
+    Text = "Get Key (Lootlab)",
+    Func = function()
+        setclipboard('https://ads.luarmor.net/get_key?for=lootLab-KHgdrTasHCWU')
+        Library:Notify({
+            Title = "LootLab has been copied!",
+            Description = "Complete the link to get Key."
+        }, 4)
     end,
 })
 
-tabKey:CreateButton({
-    Name = 'Get key (lootlab.com) (it will copy. Paste the link in browser)',
-    Callback = function()
-        setclipboard(lootlab)
+Tabs.Key:AddButton({
+    Text = "Join Discord",
+    Func = function()
+        setclipboard(discord_link)
+        Library:Notify("Discord Link has been copied", 4)
     end,
 })
+
+Tabs.Key:AddButton({
+    Text = "Rejoin Server",
+    Func = function()
+        TeleportService:Teleport(game.PlaceId)
+        Library:Notify("Rejoining ...", 4)
+    end,
+})
+
