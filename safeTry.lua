f3f790c5f8aa97e22707679c5d5a79fea7811177
raydjs <68088905+raydjs@users.noreply.github.com>
2025-07-22 03:35:56 -0600

    Update safeTry.lua

--- safeTry.lua
+++ safeTry.lua
@@ -1,8 +1,6 @@
 return function(fn, interval: number?)
     while true do task.wait(interval or .02) 
-        local success, data = pcall(function()
-            return fn()    
-        end)
+        local success, data = pcall(fn)
 
         if success then
             return data
